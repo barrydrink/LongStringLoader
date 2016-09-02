f0ce7aff8d91d31e36a59da9b8b8dba855913d1f
penrillian-barry <barry@penrillian.com>
2016-09-02 14:24:54 +0100

    Find words that have been broken by the splitter and repair them. Work in progress

--- exampleApp/LongStringLoaderExample/longStringLoader/src/androidTest/java/com/penrillian/longstringloader/LongStringLoaderTest.java
+++ exampleApp/LongStringLoaderExample/longStringLoader/src/androidTest/java/com/penrillian/longstringloader/LongStringLoaderTest.java
@@ -4,13 +4,12 @@ import android.os.Looper;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.google.common.collect.Lists;
-
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
@@ -21,14 +20,19 @@ public class LongStringLoaderTest
 	@BeforeClass
 	public static void setUp() {
 		Looper.prepare();
-		loader = new LongStringLoader(InstrumentationRegistry.getContext(), new LongStringLoadCompleteListener()
+		loader = createLoaderWithSplitLength(20);
+	}
+
+	private static LongStringLoader createLoaderWithSplitLength(int splitLength)
+	{
+		return new LongStringLoader(InstrumentationRegistry.getContext(), new LongStringLoadCompleteListener()
 		{
 			@Override
 			public void onStringLoadComplete()
 			{
 
 			}
-		}, null);
+		}, null, splitLength, 0);
 	}
 
 	@Test
@@ -40,12 +44,53 @@ public class LongStringLoaderTest
 	@Test
 	public void testGetSplitString() throws Exception
 	{
-		InstrumentationRegistry.getContext();
 		String stringToSplit = "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS";
 
-		Iterable<String> splitStringIterable = loader.getSplitString(stringToSplit);
-		List<String> splitStringList = Lists.newArrayList(splitStringIterable);
+		List<String> splitStringList = loader.getSplitString(stringToSplit);
 
 		Assert.assertTrue(splitStringList.size() > 0);
+		Assert.assertEquals("THIS SOFTWARE IS PROVIDED", splitStringList.get(0));
+
+		loader = createLoaderWithSplitLength(12);
+		splitStringList = loader.getSplitString(stringToSplit);
+
+		Assert.assertTrue(splitStringList.size() > 0);
+		Assert.assertEquals("THIS SOFTWARE", splitStringList.get(0));
+		Assert.assertEquals("IS PROVIDED", splitStringList.get(1));
+
+		loader = createLoaderWithSplitLength(1);
+		splitStringList = loader.getSplitString(stringToSplit);
+
+		Assert.assertTrue(splitStringList.size() > 0);
+		Assert.assertEquals("THIS", splitStringList.get(0));
+		Assert.assertEquals("SOFTWARE", splitStringList.get(1));
+
+	}
+
+	@Test
+	public void testFixBrokenWords() throws Exception
+	{
+		List<String> splitStringListWithBrokenWords = new ArrayList<>();
+		splitStringListWithBrokenWords.add("THIS SOFTWARE IS PROV");
+		splitStringListWithBrokenWords.add("IDED BY THE COPYRIGHT HOLDERS");
+
+		List<String> splitStringListWithFixedWords = loader.fixBrokenWords(splitStringListWithBrokenWords);
+
+		Assert.assertEquals("THIS SOFTWARE IS PROVIDED", splitStringListWithFixedWords.get(0));
+		Assert.assertEquals("BY THE COPYRIGHT HOLDERS", splitStringListWithFixedWords.get(1));
+	}
+
+	@Test
+	public void testGetStartOfBrokenString() throws Exception
+	{
+		String stringWithBrokenWord = "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLD";
+		Assert.assertEquals("HOLD", loader.getStartOfBrokenString(stringWithBrokenWord));
+	}
+
+	@Test
+	public void testGetEndOfBrokenString() throws Exception
+	{
+		String stringWithBrokenWord = "TWARE IS PROVIDED BY THE COPYRIGHT HOLDERS";
+		Assert.assertEquals("TWARE", loader.getEndOfBrokenString(stringWithBrokenWord));
 	}
 }
--- exampleApp/LongStringLoaderExample/longStringLoader/src/main/java/com/penrillian/longstringloader/LongStringLoader.java
+++ exampleApp/LongStringLoaderExample/longStringLoader/src/main/java/com/penrillian/longstringloader/LongStringLoader.java
@@ -8,6 +8,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class LongStringLoader
@@ -51,7 +56,7 @@ public class LongStringLoader
 		@Override
 		public void run()
 		{
-			Iterable<String> licenceList = getSplitString(stringToLoad);
+			List<String> licenceList = getSplitString(stringToLoad);
 			for (final String string : licenceList)
 			{
 				handler.post(new Runnable()
@@ -89,8 +94,62 @@ public class LongStringLoader
 	}
 
 	@NonNull
-	public Iterable<String> getSplitString(String stringToSplit)
+	public List<String> getSplitString(String stringToSplit)
 	{
-		return Splitter.fixedLength(stringSplitLength).split(stringToSplit);
+		return fixBrokenWords(Lists.newArrayList(Splitter.fixedLength(stringSplitLength).split(stringToSplit)));
+	}
+
+	@NonNull
+	public List<String> fixBrokenWords(List<String> splitStringList)
+	{
+		String startOfBrokenString;
+		String endOfBrokenString;
+		Matcher endStringMatcher;
+		Matcher startStringMatcher;
+		Matcher startWhiteSpaceStringMatcher;
+		for(int i = 0; i < splitStringList.size() - 1; i++)
+		{
+			endStringMatcher = Pattern.compile("\\w$", Pattern.CASE_INSENSITIVE).matcher(splitStringList.get(i));
+			startStringMatcher = Pattern.compile("^\\w", Pattern.CASE_INSENSITIVE).matcher(splitStringList.get(i+1));
+			if(endStringMatcher.find() && startStringMatcher.find())
+			{
+				startOfBrokenString = getStartOfBrokenString(splitStringList.get(i));
+				endOfBrokenString = getEndOfBrokenString(splitStringList.get(i+1));
+				splitStringList.add(i, splitStringList.get(i).replace(startOfBrokenString, startOfBrokenString + endOfBrokenString));
+				splitStringList.remove(i+1);
+
+				String s = splitStringList.get(i+1).replace(endOfBrokenString, "");
+				startWhiteSpaceStringMatcher = Pattern.compile("^\\s").matcher(s);
+				if(startWhiteSpaceStringMatcher.find())
+				{
+					s = startWhiteSpaceStringMatcher.replaceFirst("");
+
+				}
+				splitStringList.add(i+1, s);
+				splitStringList.remove(i+2);
+			}
+		}
+		return splitStringList;
+	}
+
+	@NonNull
+	public String getEndOfBrokenString(String s)
+	{
+		Matcher m = Pattern.compile("\\s", Pattern.CASE_INSENSITIVE).matcher(s);
+		if(m.find())
+		{
+			return s.substring(0, m.start()).trim();
+		}
+		return "";
+	}
+
+	public String getStartOfBrokenString(String s)
+	{
+		Matcher m = Pattern.compile("\\s\\w+$", Pattern.CASE_INSENSITIVE).matcher(s);
+		if(m.find())
+		{
+			return s.substring(m.start()).trim();
+		}
+		return "";
 	}
 }
